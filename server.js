8b647b93f3cf3c6fc0ae3c2d1f4ccfe4f298df11
Mohamed Nizar <arnizz077@gmail.com>
2025-07-03 01:00:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,9 @@ const app = express();
 // ✅ Increase payload limit to avoid PayloadTooLargeError
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-//app.use(cors());
-app.use(cors({
-  origin: 'https://civicfix-frontend-pearl.vercel.app', 
-  credentials: true,
-}));
+
+app.use(cors());
+
 
 // ✅ Routes
 app.use('/api/complaints', require('./routes/complaintRoutes'));
